f69a40066d4e891f9a4ffdb24428dba687dae097
antonsek <44634016+antonsek@users.noreply.github.com>
2025-11-02 22:55:43 +0500

    initial

---
+++ GlobalOnlinebank.Application/DTOs/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace GlobalOnlinebank.Application.DTOs
+{
+    public record ProductDto(int Id, string Name, string Description, decimal Price);
+
+    public record CreateProductDto(string Name, string Description, decimal Price);
+
+    public record UpdateProductDto(string Name, string Description, decimal Price);
+}
---
+++ GlobalOnlinebank.WebApi/Program.cs
@@ -0,0 +1,34 @@
+using GlobalOnlinebank.Application.Interfaces;
+using GlobalOnlinebank.Application.Services;
+using GlobalOnlinebank.Domain.Interfaces;
+using GlobalOnlinebank.Infrastructure.Data;
+using GlobalOnlinebank.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register dependencies And Use InMemory Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("CleanArchitectureDb"));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
